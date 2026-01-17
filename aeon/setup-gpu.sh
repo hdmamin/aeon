82baaa73a944d97bef149ff79c639ff3e40b9d51
hdmamin <hmamin2@dons.usfca.edu>
2026-01-16 23:52:17 -0800

    gpu setup script

---
+++ aeon/setup-gpu.sh
@@ -0,0 +1,8 @@
+# TODO: could mv to makefile or some new scripts dir
+# But this is a good start.
+sudo snap install astral-uv --classic
+uv venv --seed
+source .venv/bin/activate
+uv sync --group gpu
+uv pip install ipykernel
+uv run python -m ipykernel install --user --name=aeon
