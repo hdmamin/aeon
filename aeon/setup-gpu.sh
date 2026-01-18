16b7694fdc234035b301ec9493d8b954e52b6cd7
hdmamin <hmamin2@dons.usfca.edu>
2026-01-17 21:54:59 -0800

    symlink ipykernel

--- aeon/setup-gpu.sh
+++ aeon/setup-gpu.sh
@@ -4,5 +4,11 @@ sudo snap install astral-uv --classic
 uv venv --seed
 source .venv/bin/activate
 uv sync --group gpu
+
+# Create ipykernel. We store it in the cwd so it persists in lambda filesystem,
+# then symlink it so it's discoverable by jupyter.
 uv pip install ipykernel
 uv run python -m ipykernel install --prefix "$(pwd)/.jupyter" --name=aeon
+mkdir -p ~/.local/share/jupyter/kernels
+ln -s "$(pwd)/.jupyter/share/jupyter/kernels/aeon" ~/.local/share/jupyter/kernels/aeon
+
