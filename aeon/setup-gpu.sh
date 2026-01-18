4f07b7883a22e7fa960dbcd3a4ffaf677abcdf12
hdmamin <hmamin2@dons.usfca.edu>
2026-01-17 22:14:59 -0800

    active

--- aeon/setup-gpu.sh
+++ aeon/setup-gpu.sh
@@ -9,7 +9,7 @@ uv sync --group gpu
 # Create ipykernel. We store it in the cwd so it persists in lambda filesystem,
 # then symlink it so it's discoverable by jupyter.
 uv pip install ipykernel
-uv run python -m ipykernel install --prefix "$(pwd)/.jupyter" --name=aeon
+uv run --active python -m ipykernel install --prefix "$(pwd)/.jupyter" --name=aeon
 mkdir -p ~/.local/share/jupyter/kernels
 ln -s "$(pwd)/.jupyter/share/jupyter/kernels/aeon" ~/.local/share/jupyter/kernels/aeon
 
